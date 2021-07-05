1aee77c6bcd1d23bc3dc1f5ce2c2b8caeae8da64
korangko <korangko@gmail.com>
2021-07-05 10:48:33 +0900

    lenar 200705

--- app/src/main/java/com/example/lenarv03/utils/WifiConnect.java
+++ app/src/main/java/com/example/lenarv03/utils/WifiConnect.java
@@ -15,7 +15,7 @@ import static android.content.Context.WIFI_SERVICE;
 public class WifiConnect {
 
     public static Boolean LenarConnected = false;
-    String LenarSSID = "KT_GiGA_5G_Wave2_6613";
+    String LenarSSID = "T_GiGA_5G_Wave2_6613";
 
 
     public void connectLenar(Activity mActivity) {
